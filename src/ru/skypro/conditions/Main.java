75cfa4a62b33c71ce67a2b3b3dceb7f4b288eb02
AHTI6IOTIK <phsts@yandex.ru>
2022-08-05 19:28:52 +0300

    Выполнил дз по строкам

--- src/ru/skypro/conditions/Main.java
+++ src/ru/skypro/conditions/Main.java
@@ -2,6 +2,17 @@ package ru.skypro.conditions;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String firstName = "Ivanov", middleName = "Ivan", lastName = "Ivanovich";
+
+        // TASK 1
+        String fullName = String.format("%s %s %s", firstName, middleName, lastName);
+        System.out.println("ФИО сотрудника — " + fullName);
+
+        // TASK 2
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
+        // TASK 3
+        fullName = "Иванов Семён Семёнович".replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника — " + fullName);
     }
 }
